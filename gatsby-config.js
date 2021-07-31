56a488e4b097281a9763009b97514ef5c3db84c8
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-07-30 22:41:20 -0400

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
+    title: "Using GraphQL ",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
+
 const AboutPage = () => {
   return (
-    <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </Layout>
+    <Layout pageTitle="About Page">
+        <h1>About Me</h1>
+        <Link to="/">Back to Home</Link>
+        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </ Layout>
   )
 }
 export default AboutPage
