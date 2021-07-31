6632d7c8cb6dea6784bd049036903ca6b2a385a5
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-07-31 00:00:32 -0400

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,27 +6,31 @@ const BlogPage = ({ data }) => {
  console.log(data)
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-          {
-              data.allFile.nodes.map(node => (
-                  <li key={node.name}>
-                      {node.name}
-                  </li>
-              ))
-          }
-      </ul>
+      {
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+          </article>
+        ))
+      }
     </Layout>
   )
 }
 
 export const query = graphql`
-query MyQuery {
-  allFile {
-    nodes {
-      name
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
+      }
     }
   }
-}
 `
 
 export default BlogPage
